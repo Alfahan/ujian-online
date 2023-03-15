3ced596af10ee31723376f59ae1df90f96088f65
Alfahan <ali.farhan160@gmail.com>
2023-03-15 14:31:54 +0700

    Helper Mixin

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,25 @@ createInertiaApp({
   resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .mount(el)
+    //set mixins
+    .mixin({
+        methods: {
+            examTimeRangeChecker: function (start_time, end_time) {
+                return new Date() >= new Date(start_time) && new Date() <= new Date(end_time)
+            },
+
+            examTimeStartChecker: function (start_time) {
+                return new Date() < new Date(start_time)
+            },
+
+            examTimeEndChecker: function (end_time) {
+                return new Date() > new Date(end_time)
+            }
+
+        },
+    })
+    .use(plugin)
+    .mount(el)
   },
 });
 
