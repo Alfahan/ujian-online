16f7a6c0f5f4d3f12baf7fe9d5724d5809a836e0
Alfahan <ali.farhan160@gmail.com>
2023-03-16 15:07:05 +0700

    Controller data student

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,8 @@ Route::prefix('admin')->group(function() {
         Route::resource('/lessons', \App\Http\Controllers\Admin\LessonController::class, ['as' => 'admin']);
         //route resource classrooms
         Route::resource('/classrooms', \App\Http\Controllers\Admin\ClassroomController::class, ['as' => 'admin']);
+        //route resource students
+        Route::resource('/students', \App\Http\Controllers\Admin\StudentController::class, ['as' => 'admin']);
+
     });
 });
